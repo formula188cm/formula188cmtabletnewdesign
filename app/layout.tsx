d24417421c258c01cebd1b4a79e5879fe78f417e
formula188cm <formula188cm@gmail.com>
2026-01-22 20:28:08 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,6 @@ export const metadata: Metadata = {
   title: "Formula188CM - Premium Height Growth Supplement",
   description:
     "Formula188CM is a scientifically formulated height growth supplement with 100% natural ingredients. Trusted by thousands worldwide.",
-  generator: "v0.app",
 }
 
 export default function RootLayout({
@@ -22,23 +21,35 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-       <!-- Meta Pixel Code -->
-<script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '920507383865310');
-fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=920507383865310&ev=PageView&noscript=1"
-/></noscript>
-<!-- End Meta Pixel Code -->s
+        {/* Meta Pixel Code */}
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '920507383865310');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=920507383865310&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
       </head>
 
       <body className="font-sans antialiased">
@@ -47,4 +58,3 @@ src="https://www.facebook.com/tr?id=920507383865310&ev=PageView&noscript=1"
     </html>
   )
 }
-
